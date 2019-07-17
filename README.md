fe78997713c1a1ad24b9a7da3456e7407dbe8088
ruphel <ruphel@gmail.com>
2019-07-17 19:29:21 +0100

    added getRandomElement function and updated the readme.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+A collection of utility functions
+
 # utility-funcs
+
+```
+const = randEl = getRandomElement([1,2,3,4,5,6,7])
+```
---
+++ getRandomElement.js
@@ -0,0 +1,5 @@
+const getRandomElement = arr => {
+    return arr[Math.floor(Math.random() * arr.lenght)]
+}
+
+module.exports = getRandomElement;
